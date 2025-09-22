b7a12e06a40f5c113c23f18fecbcbc0e06c50b70
dy73033023 <dy73033023@tukorea.ac.kr>
2025-09-22 12:43:45 +0900

    4가지 동작 함수 뼈대 구현

--- Lecture05_Animation/animation.viewer.py
+++ Lecture05_Animation/animation.viewer.py
@@ -6,13 +6,24 @@ character = load_image('Drill04-sheet.png')
 
 # fill here
 frame = 0
-스
+
+def walk():
+    pass
+
+def run():
+    pass
+
+def rolling():
+    pass
+
+def stand():
+    pass
+
 for x in range(0,800,10):
     clear_canvas()
-
-    character.clip_draw(frame*100,0,100,100,800,90)
+    character.clip_draw(frame*100,0,44,56,400,300,500,500)
     update_canvas()
-    frame = (frame + 1)%8
+    frame = (frame + 1)%9
     delay(0.05)
 
 close_canvas()
